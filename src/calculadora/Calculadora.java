b21123104ec85c0135e7f46676dbbf917e4700b3
Luiz Henrique <luizhenrique65almeida@gmail.com>
2022-12-30 17:14:48 -0300

    Criando os componetes do display e teclado

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,5 +1,8 @@
 package calculadora;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
@@ -7,12 +10,32 @@ public class Calculadora extends JFrame {
 	
 	public Calculadora () {
 		
+		organizarLayout();
+		
 		setSize(232, 322); /* Tamanho da tela*/
 		setDefaultCloseOperation(HIDE_ON_CLOSE); /* Faz encerrar o programa ao fechar a tela*/
+		setLocationRelativeTo(null); /*  tela no centro*/
 		setVisible(true); /* Faz aparecer uma tela*/
 		
 	}
 	
+	
+	
+	
+	private void organizarLayout() {
+		setLayout(new BorderLayout());
+		
+		Display display = new Display();
+		display.setPreferredSize(new Dimension(233, 60));
+		add(display, BorderLayout.NORTH);
+		
+		Teclado teclado = new Teclado();
+		add(teclado, BorderLayout.CENTER);
+	}
+
+
+
+
 	public static void main(String[] args) {
 		
 		new Calculadora();
---
+++ src/calculadora/Display.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Display extends JPanel {
+	
+//	Jpanel = containers
+	
+	
+	public Display () {
+	setBackground(Color.BLUE);
+}
+}
